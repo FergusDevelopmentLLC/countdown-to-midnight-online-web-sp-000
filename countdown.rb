3a42e8068a656765de97d4c5635b7f729dbb728d
Will Carter <will.carter@fergusllc.com>
2019-12-18 17:58:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
-def countdown(counter)
-  while counter > 0
-    puts counter
+def countdown(number)
+  while number > 0
+    "#{number} SECOND(S)!"
     counter -= 1
   end
 end
